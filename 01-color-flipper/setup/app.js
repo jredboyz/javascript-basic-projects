4290d5ab4a24811d9b7a0baf4aa09ca81558b67b
Joseph <jredboyz@gmail.com>
2023-11-15 11:39:26 -0800

    completed projects 1 - 7

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+  // random number between 0 - 3
+  const RandomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[RandomNumber];
+  color.textContent = colors[RandomNumber];
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length)
+}
